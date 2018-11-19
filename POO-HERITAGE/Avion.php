9542d8ccac0e32036296e9668183dc3ba98aab37
heytehm <44373701+heytehm@users.noreply.github.com>
2018-11-19 14:44:07 +0100

    Update Avion.php

--- POO-HERITAGE/Avion.php
+++ POO-HERITAGE/Avion.php
@@ -8,25 +8,39 @@
 
 class Avion extends Vehicule{
     private $nombreMoteurs;
+
+    /**
+     * @return mixed
+     */
+    public function getNombreMoteurs()
+    {
+        return $this->nombreMoteurs;
+    }
+
+    /**
+     * @param mixed $nombreMoteurs
+     */
+    public function setNombreMoteurs($nombreMoteurs)
+    {
+        $this->nombreMoteurs = $nombreMoteurs;
+    }
     public function décoller(){
             }
-    public function __construct($marque, $dateAchat, $prixAchat,$nombreMoteurs) {
+    public function __construct($marque, $prixAchat,$nombreMoteurs,$prixCourant) {
 
-        Vehicule::__construct($marque, $dateAchat, $prixAchat);
+        echo ' constructeur de la classe Avion<br>'; // Message s'affichant une fois que tout objet est créé.
+        $this->setNombreMoteurs($nombreMoteurs);
+        Vehicule::__construct($marque, $prixAchat,$prixCourant);
     }
     public function afficheValeurAttributs() //méthode publique affiche() qui affiche la valeur des attributs.
     {
-        echo $this->_marque;
-        echo $this->_dateAchat;
-        echo $this->_prixAchat;
-        echo $this->_nombreMoteurs;
+        echo "la marque de l'avion est:".$this->marque.'<br>';
+        echo " leprix d'achat de l'avion est: ".$this->prixAchat.'<br>';
+        echo " leprix de vente de l'avion est: ".$this->prixCourant.'<br>';
+        echo "le nombre de moteurs est :".$this->nombreMoteurs.'<br>';
     }
-    function déplacer( )
+    function deplacer( )
     {
 
     }
-
-
-
-
 }
